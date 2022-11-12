bd46e88cf765f772dbc52676fbdf51007e43f8ad
fimercadog <fimercadog@gmail.com>
2022-11-12 08:35:24 -0500

    modiciacion models user

--- ferretic/models.py
+++ ferretic/models.py
@@ -2,17 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import PROTECT
 
-class User(AbstractUser): # To validated users
-    employee_name = models.CharField(max_length=50)
-    employee_lastname = models.CharField(max_length=50)
-    employee_address = models.CharField(max_length=100)
-    employee_telephone = models.CharField(max_length=15)
-    employee_birthday = models.CharField(max_length=15)
-    employee_username = models.CharField(max_length=80)
-    employee_password = models.CharField(max_length=30)
+
+class User(AbstractUser):  # To validated users
+    user_name = models.CharField(max_length=50)
+    user_lastname = models.CharField(max_length=50)
+    user_address = models.CharField(max_length=100)
+    user_telephone = models.CharField(max_length=15)
+    user_birthday = models.CharField(max_length=15)
+    user_username = models.CharField(max_length=80)
+    user_password = models.CharField(max_length=30)
     token = models.CharField(max_length=100, default='', null=True, blank=True)
 
-class Employee(models.Model): # Tabla de emppleado
+
+class Employee(models.Model):  # Tabla de emppleado
     employee_name = models.CharField(max_length=50)
     employee_lastname = models.CharField(max_length=50)
     employee_address = models.CharField(max_length=100)
@@ -20,42 +22,52 @@ class Employee(models.Model): # Tabla de emppleado
     employee_birthday = models.CharField(max_length=15)
     employee_username = models.CharField(max_length=80)
     employee_password = models.CharField(max_length=30)
+
     def __str__(self):
         return self.employee_name + ' ' + self.employee_lastname
 
-class Vendor(models.Model): # Tabla de proveedores
+
+class Vendor(models.Model):  # Tabla de proveedores
     vendor_name = models.CharField(max_length=80)
     vendor_address = models.CharField(max_length=100)
     vendor_telephone = models.CharField(max_length=15)
     vendor_email = models.EmailField()
+
     def __str__(self):
         return self.vendor_name
 
-class Client(models.Model): # Tabla de clientes
+
+class Client(models.Model):  # Tabla de clientes
     client_name = models.CharField(max_length=50)
     client_lastname = models.CharField(max_length=50)
     client_address = models.CharField(max_length=100)
     client_telephone = models.CharField(max_length=15)
     client_email = models.EmailField()
+
     def __str__(self):
         return self.client_name + ' ' + self.client_lastname
 
-class Product(models.Model): # Tabla de productos
+
+class Product(models.Model):  # Tabla de productos
     product_name = models.CharField(max_length=50)
     product_description = models.TextField(max_length=80)
     product_price = models.FloatField()
     product_stock = models.IntegerField()
+
     def __str__(self):
         return self.product_name
 
-class Order(models.Model): # Tabla de pedidos de productos
+
+class Order(models.Model):  # Tabla de pedidos de productos
     vendor = models.ForeignKey(Vendor, on_delete=PROTECT)
     employee = models.ForeignKey(Employee, on_delete=PROTECT)
     order_number = models.CharField(max_length=12)
+
     def __str__(self):
         return self.order_number
 
-class Order_product(models.Model): # Tabla de relacion pedidos y productos
+
+class Order_product(models.Model):  # Tabla de relacion pedidos y productos
     order = models.ForeignKey(Order, on_delete=PROTECT)
     order_date = models.DateTimeField(auto_now_add=True)
     product = models.ForeignKey(Product, on_delete=PROTECT)
@@ -65,14 +77,17 @@ class Order_product(models.Model): # Tabla de relacion pedidos y productos
     '''def __str__(self):
         return self.order_number'''
 
-class Invoice(models.Model): # Tabla de facturas
+
+class Invoice(models.Model):  # Tabla de facturas
     client = models.ForeignKey(Client, on_delete=PROTECT)
     employee = models.ForeignKey(Employee, on_delete=PROTECT)
     invoice_number = models.CharField(max_length=12)
+
     def __str__(self):
         return self.invoice_number
 
-class Invoice_product(models.Model): # Tabla de relacion facturas y productos
+
+class Invoice_product(models.Model):  # Tabla de relacion facturas y productos
     invoice = models.ForeignKey(Invoice, on_delete=PROTECT)
     invoice_date = models.DateTimeField(auto_now_add=True)
     product = models.ForeignKey(Product, on_delete=PROTECT)
